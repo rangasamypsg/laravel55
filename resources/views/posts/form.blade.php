f60b764047c6c5fd898afa9319936847a8a34def
Rangasamy <rangasamy.psg@gmail.com>
2018-03-05 10:53:33 +0530

    first commit

---
+++ resources/views/posts/form.blade.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Title:</strong>
+            {!! Form::text('title', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Body:</strong>
+            {!! Form::text('body', null, array('placeholder' => 'Body','class' => 'form-control')) !!}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</div>
